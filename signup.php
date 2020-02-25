177ba8d1cffac9e731c28ffff2770ec2907602ff
mbohn28 <35979720+mbohn28@users.noreply.github.com>
2020-02-25 14:58:29 -0500

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,38 @@
+<?php>
+<html>
+
+<form action="/action_page.php" method="post">
+	
+	<div class="signup">
+		<label for="fname"><b>First Name</b></label>
+		<input type="text" placeholder="Enter First Name" name="fname" required>
+
+		<br>
+
+		<label for="lname"><b>Last Name</b></label>
+		<input type="text" placeholder="Enter Last Name" name="fname" required>
+
+		<br>
+
+		<label for="email"><b>Email</b></label>
+		<input type="text" placeholder="Enter Email Address" name="email" required>
+
+		<br>
+
+		<label for="psw"><b>Password</b></label>
+		<input type="password" placeholder="Enter Password" name="psw" required>
+
+		<br>
+
+		<label for="psw"><b>Re-enter Password</b></label>
+		<input type="password" placeholder="Enter Password" name="psw" required>
+
+		<button type="submit">Sign Up </button>
+		
+		</div>
+
+
+</html>
+
+
+<php?>

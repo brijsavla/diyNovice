a5fa024b2242e6a3ac1b4e8d4f7c4b4bb02ba7cd
Leonidous <43423871+Leonidous@users.noreply.github.com>
2020-04-10 20:51:03 -0400

    Add files via upload

---
+++ Front/Anthony/resources/views/inc/navbar.blade.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-inverse">
+    <div class="page-header"></div>
+    <div class="navbar-container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                DiyNovice.com
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                &nbsp;
+            </ul>
+
+            <ul class="nav navbar-nav">
+              <li><a href="/about">Visit: BoutIt.com</a></li>
+              <li><a href="/services">Rennovation Estimator</a></li>
+              <li><a href="/posts">Other Featured System Resources</a></li>
+              <li><a href="/posts">Login / Account Information</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ Front/Anthony/resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="grid-container">
+        <div>This box is reserved for Sponsor/Advertiser and/or Estimator Supplier banner ads/promotions.</div>
+        <div>Books and Resouces by: Will G. Louden</div>
+        <div>This box is reserved for Site/network announcements, site/network promotions, product promotions and/or "other" information that the company wants to get the word out on etc.</div>  
+    </div>
+@endsection
